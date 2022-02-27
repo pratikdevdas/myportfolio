e7c8e09acd3a229bafae718ead214d7438d9923e
rev <dasdev.pratik@gmail.com>
2022-02-27 18:49:27 +0530

    Big fixes

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,9 @@
 import '../styles/globals.css'
-import Navbar from '../components/Navbar'
-import Middle from './Middle'
-import Footer from '../components/Footer'
-import Home from './Home'
+
 
 function MyApp({ Component, pageProps }) {
-  return (
-  <div>
-    <Navbar></Navbar>
-    <Home></Home>
-    <Footer></Footer>
-  </div>
-    )
+  return <Component {...pageProps} />
 }
+
 
 export default MyApp
