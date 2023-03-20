047921fbc46a9ea621746fd0875ea55656e1548c
Revenant <dasdev.pratik@gmail.com>
2023-03-21 03:46:41 +0530

    disabled Blog

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,8 @@ import Hero from "../components/Layout/Hero";
 import Navbar from "../components/Layout/Navbar";
 import Footer from "../components/Layout/Footer";
 import Projects from "../components/Projects";
-import {promises as fsPromises} from 'fs';
-import path from 'path'
+import { promises as fsPromises } from "fs";
+import path from "path";
 
 export default function Home(props) {
   return (
@@ -19,15 +19,29 @@ export default function Home(props) {
           <Navbar />
           <div className="mx-auto  max-w-[660px] lg:max-w-[900px]   xl:max-w-[1100px]">
             <Hero />
-            <Projects projects={props.projects}/>
+            <Projects projects={props.projects} />
           </div>
         </div>
         <div className="mx-auto  max-w-[660px] lg:max-w-[900px]   xl:max-w-[1100px]">
           <Footer />
         </div>
-        <div className="flex justify-center text-lg md:text-xl  text-center text-light border-t-g700 border-t-2">
-         <p className="py-3">
-         Made with ❤️ by <a href="https://www.pratikdevdas.com" className="underline pl-1 text-green hover:text-light"> Pratik.</a> Read my blogs <a href="https://blog.pratikdevdas.com/" className="underline pl-1 text-green hover:text-light" >here.</a></p> 
+        <div className="flex justify-center border-t-2 border-t-g700  text-center text-lg text-light md:text-xl">
+          <p className="py-3">
+            Made with ❤️ by{" "}
+            <a
+              href="https://www.pratikdevdas.com"
+              className="pl-1 text-green underline hover:text-light"
+            >
+              Pratik.
+            </a>{" "}
+            {/* Read my blogs{" "}
+            <a
+              href="https://blog.pratikdevdas.com/"
+              className="pl-1 text-green underline hover:text-light"
+            >
+              here.
+            </a> */}
+          </p>
         </div>
       </div>
     </div>
@@ -35,10 +49,10 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'data.json');
+  const filePath = path.join(process.cwd(), "data.json");
   const jsonData = await fsPromises.readFile(filePath);
   const objectData = JSON.parse(jsonData);
   return {
-    props: objectData
-  }
+    props: objectData,
+  };
 }
