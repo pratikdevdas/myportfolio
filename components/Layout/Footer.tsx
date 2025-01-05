c9cf8a979be7e5df5dc12ea4e5705fc69df242eb
Pratik Dev Das <dasdev.pratik@gmail.com>
2025-01-05 23:48:37 +0530

    fix: ✅ update footer

--- components/Layout/Footer.tsx
+++ components/Layout/Footer.tsx
@@ -3,28 +3,20 @@ import {
   LinkedIn,
   Twitter,
   Youtube,
-  // Hashnode,
 } from "../../public/icons/Icons";
 
 function Footer() {
   return (
-    <div className="flex flex-col items-center justify-between px-8 py-12 text-light md:px-12 lg:flex-row">
-      <div className="mb-8 text-center lg:mb-0 lg:text-left">
-        <h1 className="text-2xl font-bold mb-3">Let&apos;s Connect</h1>
-        <a
-          title="Send me an email"
-          href="mailto:pratikdevdas@outlook.com"
-          className="text-lg hover:text-dark-green-fluorescent transition-colors"
-        >
-          pratikdevdas@outlook.com
-        </a>
-      </div>
-      <div className="flex gap-8">
+    <div className="flex flex-col items-center justify-center py-12 text-light">
+      <p className="text-2xl mb-6">
+        I like Cooking Code & Learning everyday.
+      </p>
+      <div className="flex gap-4">
         {[
           { Icon: Twitter, href: "https://twitter.com/pratikdevdas" },
-          { Icon: Github, href: "https://github.com/pratikdevdas" },
+          { Icon: Youtube, href: "https://www.youtube.com/@pratikdevdas" },
           { Icon: LinkedIn, href: "https://www.linkedin.com/in/pratikdevdas/" },
-          { Icon: Youtube, href: "https://www.youtube.com/@pratikdevdas" }
+          { Icon: Github, href: "https://github.com/pratikdevdas" },
         ].map(({ Icon, href }) => (
           <a
             key={href}
